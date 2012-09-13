470b69e0b11e9ffde9000b5e8232d0bd289a63e9
abhilash_laptop <admin@havishaa.com>
2012-09-14 01:02:33 +0530

    changed to php

--- about_us.php
+++ about_us.php
@@ -8,8 +8,8 @@
 <div class="logo"><img src="images/new_logo.png"></img></div>
 <div class="nav-bar">
 <ul>
-<li><a href="index.html">Home</a></li>
-<li><a href="about_us.html">About Us</a></li>
+<li><a href="index.php">Home</a></li>
+<li><a href="about_us.php">About Us</a></li>
 <li><a href="#">We Offer</a></li>
 <li><a href="#">Why Us</a></li>
 <li><a href="#">Careers</a></li>
@@ -17,11 +17,18 @@
 <ul>
 </div>
 <div class="content">
-<div class="home-content">
-Havishaa is on air!!!<br />
-06/09/2012<br />
-<br /><br /><br />
-</div>
+	
+<p>
+	Havishaa is a Bangalore based Technology Services & Solutions Company. We deliver State-Of-The_Art services to clients globally. We have a healthy range of customers ranging from Startups to  MNC's.
+</p>
+
+<p>
+	We provide 24X7 Support services catering to all layers of technology including Business Process Outsourcing, Technical Support,  On-Call Support etc.
+</p>
+<p>
+	Our prime focus is on customer satisfaction and delivery quality.
+</p>
+
 </div>
 <div class="nav-bar">
 <ul>
@@ -32,8 +39,9 @@ Havishaa is on air!!!<br />
 <li><a href="#">Careers</a></li>
 <li><a href="#">Contact Us</a></li>
 <ul>
-<div class="copyright"> © 2012 Havisha</div>
+<div class="copyright"> ï¿½ 2012 Havisha</div>
 </div>
 </div>
 </body>
 </html>
+
---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+	<head>
+		<title>Havishaa</title>
+		<link rel="stylesheet" href="css/main.css">
+		</link>
+	</head>
+	<body>
+		<div class="container">
+			<div class="logo"><img src="images/new_logo.png"></img>
+			</div>
+			<div class="nav-bar">
+				<ul>
+					<li>
+						<a href="index.php">Home</a>
+					</li>
+					<li>
+						<a href="about_us.php">About Us</a>
+					</li>
+					<li>
+						<a href="#">We Offer</a>
+					</li>
+					<li>
+						<a href="#">Why Us</a>
+					</li>
+					<li>
+						<a href="#">Careers</a>
+					</li>
+					<li>
+						<a href="#">Contact Us</a>
+					</li>
+					<ul>
+			</div>
+			<div class="content">
+				<div class="home-content">
+					<div>
+						<img src="images/about_us3.png" />
+					</div>
+					<div class="main-content">
+						<p>
+							Havishaa is a Bangalore based Technology Services & Solutions Company. We deliver State-Of-The_Art services to clients globally. We have a healthy range of customers ranging from Startups to  MNC's.
+						</p>
+
+						<p>
+							We provide 24X7 Support services catering to all layers of technology including Business Process Outsourcing, Technical Support,  On-Call Support etc.
+						</p>
+						<p>
+							Our prime focus is on customer satisfaction and delivery quality.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="nav-bar">
+				<ul>
+					<li>
+						<a href="index.html">Home</a>
+					</li>
+					<li>
+						<a href="about_us.html">About Us</a>
+					</li>
+					<li>
+						<a href="#">We Offer</a>
+					</li>
+					<li>
+						<a href="#">Why Us</a>
+					</li>
+					<li>
+						<a href="#">Careers</a>
+					</li>
+					<li>
+						<a href="#">Contact Us</a>
+					</li>
+					<ul>
+						<div class="copyright">
+							� 2012 Havisha
+						</div>
+			</div>
+		</div>
+	</body>
+</html>
